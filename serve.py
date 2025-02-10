5cf1fa88e2950ecd83ae2eac981869b3da1abb6c
Nikolay Kostadinov <nkkostadinov@gmail.com>
2025-02-10 11:01:01 +0100

    Updated serve to use latest vllm engine

--- serve.py
+++ serve.py
@@ -1,38 +1,207 @@
+# __serve_example_begin__
+from typing import Dict, Optional, List
 import logging
-import os
-from typing import Any, Dict
 
 from fastapi import FastAPI
 from starlette.requests import Request
-
-# from ray import serve
+from starlette.responses import StreamingResponse, JSONResponse
+import os
 from ray import serve
-from qwentransformers import Qwen2dot5
 
-# Configure logging
-logger = logging.getLogger("ray.serve")
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
+from vllm.entrypoints.openai.cli_args import make_arg_parser
+from vllm.entrypoints.openai.protocol import (
+    ChatCompletionRequest,
+    ChatCompletionResponse,
+    ErrorResponse,
+)
+from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
+from vllm.entrypoints.openai.serving_models import (
+    BaseModelPath,
+    LoRAModulePath,
+    PromptAdapterPath,
+    OpenAIServingModels,
+)
+
+from vllm.utils import FlexibleArgumentParser
+from vllm.entrypoints.logger import RequestLogger
 
 logger = logging.getLogger("ray.serve")
 
 app = FastAPI()
 
-@serve.deployment(name="Qwen2dot5Deployment", num_replicas=1, ray_actor_options={"num_gpus": 1})
-@serve.ingress(app)
-class Qwen2dot5Deployment:
-    def __init__(
-            self,
-            model_name: str
-    ):
-        self.qwen = Qwen2dot5(logger, model_name)
 
-    @app.post("/v1/chat/completions", response_model=Any)
-    async def create_chat_completions(self, raw_request: Request):
-        request_body = await raw_request.json()
-        return self.qwen.create_chat_completions(request_body)
+@serve.deployment(
+    autoscaling_config={
+        "min_replicas": 1,
+        "max_replicas": 10,
+        "target_ongoing_requests": 5,
+    },
+    max_ongoing_requests=10,
+)
+@serve.ingress(app)
+class VLLMDeployment:
+    def __init__(
+        self,
+        engine_args: AsyncEngineArgs,
+        response_role: str,
+        lora_modules: Optional[List[LoRAModulePath]] = None,
+        prompt_adapters: Optional[List[PromptAdapterPath]] = None,
+        request_logger: Optional[RequestLogger] = None,
+        chat_template: Optional[str] = None,
+    ):
+        logger.info(f"Starting with engine args: {engine_args}")
+        self.openai_serving_chat = None
+        self.engine_args = engine_args
+        self.response_role = response_role
+        self.lora_modules = lora_modules
+        self.prompt_adapters = prompt_adapters
+        self.request_logger = request_logger
+        self.chat_template = chat_template
+        self.engine = AsyncLLMEngine.from_engine_args(engine_args)
+
+    @app.post("/v1/chat/completions")
+    async def create_chat_completion(
+        self, request: ChatCompletionRequest, raw_request: Request
+    ):
+        """OpenAI-compatible HTTP endpoint.
+
+        API reference:
+            - https://docs.vllm.ai/en/latest/serving/openai_compatible_server.html
+        """
+        if not self.openai_serving_chat:
+            model_config = await self.engine.get_model_config()
+            # Determine the name of the served model for the OpenAI client.
+            models = OpenAIServingModels(
+                self.engine,
+                model_config,
+                [
+                    BaseModelPath(
+                        name=self.engine_args.model, model_path=self.engine_args.model
+                    )
+                ],
+                lora_modules=self.lora_modules,
+                prompt_adapters=self.prompt_adapters,
+            )
+            self.openai_serving_chat = OpenAIServingChat(
+                self.engine,
+                model_config,
+                models,
+                self.response_role,
+                request_logger=self.request_logger,
+                chat_template=self.chat_template,
+                chat_template_content_format="auto",
+            )
+        logger.info(f"Request: {request}")
+        generator = await self.openai_serving_chat.create_chat_completion(
+            request, raw_request
+        )
+        if isinstance(generator, ErrorResponse):
+            return JSONResponse(
+                content=generator.model_dump(), status_code=generator.code
+            )
+        if request.stream:
+            return StreamingResponse(content=generator, media_type="text/event-stream")
+        else:
+            assert isinstance(generator, ChatCompletionResponse)
+            return JSONResponse(content=generator.model_dump())
+
+
+def parse_vllm_args(cli_args: Dict[str, str]):
+    """Parses vLLM args based on CLI inputs.
+
+    Currently uses argparse because vLLM doesn't expose Python models for all of the
+    config options we want to support.
+    """
+    arg_parser = FlexibleArgumentParser(
+        description="vLLM OpenAI-Compatible RESTful API server."
+    )
+
+    parser = make_arg_parser(arg_parser)
+    arg_strings = []
+    for key, value in cli_args.items():
+        arg_strings.extend([f"--{key}", str(value)])
+    logger.info(arg_strings)
+    parsed_args = parser.parse_args(args=arg_strings)
+    return parsed_args
 
 
 def build_app(cli_args: Dict[str, str]) -> serve.Application:
-    return Qwen2dot5Deployment.bind(cli_args['model'])
+    """Builds the Serve app based on CLI arguments.
 
+    See https://docs.vllm.ai/en/latest/serving/openai_compatible_server.html#command-line-arguments-for-the-server
+    for the complete set of arguments.
 
-model = build_app({"model": os.environ['MODEL_ID']})
+    Supported engine arguments: https://docs.vllm.ai/en/latest/models/engine_args.html.
+    """  # noqa: E501
+    if "accelerator" in cli_args.keys():
+        accelerator = cli_args.pop("accelerator")
+    else:
+        accelerator = "GPU"
+    parsed_args = parse_vllm_args(cli_args)
+    engine_args = AsyncEngineArgs.from_cli_args(parsed_args)
+    engine_args.worker_use_ray = True
+
+    tp = engine_args.tensor_parallel_size
+    logger.info(f"Tensor parallelism = {tp}")
+    pg_resources = []
+    pg_resources.append({"CPU": 1})  # for the deployment replica
+    for i in range(tp):
+        pg_resources.append({"CPU": 1, accelerator: 1})  # for the vLLM actors
+
+    # We use the "STRICT_PACK" strategy below to ensure all vLLM actors are placed on
+    # the same Ray node.
+    return VLLMDeployment.options(
+        placement_group_bundles=pg_resources, placement_group_strategy="STRICT_PACK"
+    ).bind(
+        engine_args,
+        parsed_args.response_role,
+        parsed_args.lora_modules,
+        parsed_args.prompt_adapters,
+        cli_args.get("request_logger"),
+        parsed_args.chat_template,
+    )
+
+model = build_app(
+    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']})
+
+# # __serve_example_end__
+#
+# if __name__ == "__main__":
+#     serve.run(
+#         build_app(
+#             {
+#                 "model": "NousResearch/Meta-Llama-3-8B-Instruct",
+#                 "tensor-parallel-size": "1",
+#             }
+#         )
+#     )
+#     # __query_example_begin__
+#     from openai import OpenAI
+#
+#     # Note: Ray Serve doesn't support all OpenAI client arguments and may ignore some.
+#     client = OpenAI(
+#         # Replace the URL if deploying your app remotely
+#         # (e.g., on Anyscale or KubeRay).
+#         base_url="http://localhost:8000/v1",
+#         api_key="NOT A REAL KEY",
+#     )
+#     chat_completion = client.chat.completions.create(
+#         model="NousResearch/Meta-Llama-3-8B-Instruct",
+#         messages=[
+#             {"role": "system", "content": "You are a helpful assistant."},
+#             {
+#                 "role": "user",
+#                 "content": "What are some highly rated restaurants in San Francisco?'",
+#             },
+#         ],
+#         temperature=0.01,
+#         stream=True,
+#         max_tokens=100,
+#     )
+#
+#     for chat in chat_completion:
+#         if chat.choices[0].delta.content is not None:
+#             print(chat.choices[0].delta.content, end="")
+#     # __query_example_end__
