bca72aaef950be4b9974f382b1b53bdd4208b757
nikolay.kostadinov <nk@jquad.de>
2025-03-10 22:03:37 +0100

    Modified serve.py to accept dtype

--- serve.py
+++ serve.py
@@ -172,7 +172,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     )
 
 model = build_app(
-    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']})
+    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM'], "dtype": os.environ['VLLM_DTYPE']})
 
 # # __serve_example_end__
 #
