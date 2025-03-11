b5eaecbad2653aff12733bf48cf4c7a570c85831
nikolay.kostadinov <nk@jquad.de>
2025-03-11 13:00:24 +0100

    enable-auto-tool-choice: True

--- serve.py
+++ serve.py
@@ -172,7 +172,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     )
 
 model = build_app(
-    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM'], "dtype": os.environ['VLLM_DTYPE'], "quantization": os.environ['VLLM_QUANTIZATION'], "device": os.environ['VLLM_DEVICE'], "gpu-memory-utilization": os.environ['VLLM_GPU_MEMORY_UTILIZATION'], "max-model-len": os.environ["VLLM_MAX_MODEL_LEN"], "enable-auto-tool-choice --tool-call-parser": "hermes"})
+    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM'], "dtype": os.environ['VLLM_DTYPE'], "quantization": os.environ['VLLM_QUANTIZATION'], "device": os.environ['VLLM_DEVICE'], "gpu-memory-utilization": os.environ['VLLM_GPU_MEMORY_UTILIZATION'], "max-model-len": os.environ["VLLM_MAX_MODEL_LEN"], "enable-auto-tool-choice": "True", "tool-call-parser": "hermes"})
 
 # # __serve_example_end__
 #
